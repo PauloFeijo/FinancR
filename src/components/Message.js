263eed61f0cb4539cc75407f852f1c869e455b3c
PauloFeijo <paulorikrdo.89@gmail.com>
2020-03-19 22:05:06 -0300

    Componente de mensagem (Snackbar)

---
+++ src/components/Message.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import Snackbar from '@material-ui/core/Snackbar'
+import IconButton from '@material-ui/core/IconButton'
+
+export default class Message extends Component {
+
+    render() {
+        return (
+            <Snackbar
+                {...this.props}
+                open={this.props.opened}
+                anchorOrigin={{ vertical: 'center', horizontal: 'center' }}
+                autoHideDuration={3000}
+                message={<span id="message-id">{this.props.message}</span>}
+                action={[
+                    <IconButton
+                        key="close"
+                        arial-label="Close"
+                        color="inherit"
+                        onClick={this.props.onClose}
+                    >x</IconButton>
+                ]}
+            />
+
+        )
+    }
+}     
