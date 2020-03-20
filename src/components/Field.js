2b4d416875e9d6566cd49d81d9cbb7391c9166a9
PauloFeijo <paulorikrdo.89@gmail.com>
2020-03-19 22:07:40 -0300

    Componente Field

---
+++ src/components/Field.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { Row, Col, Form, } from 'react-bootstrap'
+
+export default class Field extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <Row>
+                <Col sm={this.props.size}>
+                    <Form.Group controlId={this.props.name}>
+                        <Form.Label>{this.props.label}</Form.Label>
+                        <Form.Control
+                            type="text"
+                            {...this.props}
+                            placeholder={this.props.label}
+                        />
+                    </Form.Group>
+                </Col>
+            </Row>
+        )
+    }
+}
