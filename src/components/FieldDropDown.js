723743c8d1b36df568fc6bf46f95be1af5c4203c
PauloFeijo <paulorikrdo.89@gmail.com>
2020-04-16 23:17:04 -0300

    Componente FieldDropDown

---
+++ src/components/FieldDropDown.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Row, Col, Form, Dropdown } from 'react-bootstrap'
+
+export default class FieldDropDown extends Component {
+    render() {
+        return (
+            <Row>
+                <Col sm={this.props.size}>
+                    <Form.Group controlId={this.props.name}>
+                        <Form.Label>{this.props.label}</Form.Label>
+                        <Form.Control as="select" {...this.props}>
+                            {this.props.options.map(option => <option>{option}</option>)}
+                        </Form.Control>
+                    </Form.Group>
+                </Col>
+            </Row>
+        )
+    }
+}
